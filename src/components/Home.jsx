b8dad2d44763c2a6b7da92cf65de875c6c27fe69
Masohas1 <jarekbabiak3@gmail.com>
2023-11-24 00:24:44 +0100

    Tworze strony pod React Router

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
-        <div className='home-container'>
+        <div className='home-container'>s
             <img className='home-photo' src="../../public/images/wallpaper.jpg" alt="" />
             <h1>PS5 games galore, adventure at the store</h1>
             <p>Embark on a gaming odyssey at our premier PS5 Haven – your go-to destination for the latest and greatest in PlayStation excitement. Immerse yourself in a carefully curated selection of top-tier games and accessories. From pulse-pounding adventures to immersive storytelling, we offer a haven for every gamer. Step into the future of gaming with cutting-edge technology and unparalleled service. Your epic journey starts here – welcome to a new era of digital delight!</p>
